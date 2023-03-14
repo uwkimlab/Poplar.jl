de8107a3eb52ff6d8bf8b4e0509850c5710c7a8e
Jun Hyuk Jeon <jnhykjeon@gmail.com>
2023-03-14 08:08:15 -0700

    Change order of file loading

--- src/Poplar.jl
+++ src/Poplar.jl
@@ -2,12 +2,12 @@ module Poplar
 
 using Cropbox
 
+include("utils/utils.jl")
 include("atmosphere/atmosphere.jl")
 include("morphology/morphology.jl")
 include("phenology/phenology.jl")
 include("physiology/physiology.jl")
 include("rhizosphere/rhizosphere.jl")
-include("utils/utils.jl")
 
 @system Model(Atmosphere, Morphology, Phenology, Physiology, Rhizosphere, Controller)
 
