12819c0c28681f1a444e55fa1dae6ac647f90ed9
junhyukjeon <jnhykjeon@gmail.com>
2023-06-07 11:32:20 -0700

    Clean up configurations

--- src/config/config.jl
+++ src/config/config.jl
@@ -24,11 +24,11 @@ config_Calendar = @config(
 # Weather data must be included.
 config_Atmosphere = @config(
     :Atmosphere => (
-        data = Poplar.loadwea(Poplar.datapath("2007.wea"), tz"Asia/Seoul")
+        data = Poplar.loadwea(Poplar.datapath("2007.wea"), tz"Asia/Seoul"),
     )
 )
 
-# Default parameters for GasExchange.
+# Default parameters for GasExchange (WIP).
 config_GasExchange_NI = @config(
     :GasExchange => (
         Tp25 = 11.55,
@@ -50,7 +50,7 @@ config_GasExchange_NI = @config(
     )
 )
 
-# GasExchange parameters for endophyte inoculation.
+# GasExchange parameters for endophyte inoculation (WIP).
 config_GasExchange_EI = @config(
     :GasExchange => (
         Tp25 = 11.55,
@@ -72,97 +72,10 @@ config_GasExchange_EI = @config(
     )
 )
 
-# 
-config_Model_Eucalyptus_globulus = @config(
-    :Model => ( 
-        # BiomassPartition
-        # FR = 1,
-        # pFS2 = 1,
-        # pFS20 = 0.15,
-        aWs = 0.095,
-        nWs = 2.4,
-        # pRx = 0.8,
-        # pRn = 0.25,
-        # m0 = 0,
-        coeffCond = 0.05,
-
-        # Mortality
-        gammaN1 = 3,
-        gammaN0 = 0,
-        tgammaN = 3,
-        ngammaN = 1,
-        wSx1000 = 400,
-        thinPower = 1.5,
-        mF = 0,
-        mR = 0.2,
-        mS = 0.2,
-         
-        # Tree
-        iStemNo= 1111,
-        fracBB0 = 0.75,
-        fracBB1 = 0.15,
-        tBB = 2,
-        rho0 = 0.450,
-        rho1 = 0.450,
-        tRho = 4,
-        aH = 0,
-        nHB = 0,
-        nHN = 0,
-        aV = 0,
-        nVB = 0,
-        nVN = 0,
-         
-        # Foliage
-        iWF = 2000,
-        SLA0= 11,
-        SLA1= 4,
-        tSLA= 2.5,
-        gammaF1= 0.027,
-        gammaF0= 0.001,
-        tgammaF= 12,
-        leaf_width= 3,
-         
-        # Stem
-        iWS = 1000,
-         
-        # Root
-        iWR = 2000,
-        gammaR= 0.015,
-
-        # Radiation
-        leaf_angle = ellipsoidal,
-        LAF = 3,
-        scattering = 0.15,
-        clumping = 1.0,
-
-        # Age
-        iAge = 1,
-        maxAge = 50,
-        nAge = 4,
-        rAge = 0.95,
-
-        # WaterBalance
-        iASW = 999,
-        maxASW = 300,
-        minASW = 0,
-        irrigation = 0,
-        pool_fraction = 0,
-        maxInterception = 0.15,
-        LAImaxInterception = 0,
-        SWconst0 = 0.7,
-        SWpower0 = 9,
-
-        # Soil
-        soil_class = CL,
-    )
-)
-
-config_1 = @config(
+config = @config(
     config_Clock,
     config_Calendar,
-    config_Atmosphere,
-    config_GasExchange_NI,
-    config_Model_Eucalyptus_globulus
+    config_Atmosphere
 )
 
 config_defoliation = @config(
@@ -188,58 +101,6 @@ config_coppicing = @config(
             ZonedDateTime(2008, 12, 31, tz"Asia/Seoul"),
             ZonedDateTime(2009, 12, 31, tz"Asia/Seoul"),
         ],
-    )
-)
-
-config_Model_Amichev = @config(
-    :Model => (
-        # BiomassPartition
-        # pFS2 = 0.8567,
-        # pFS20 = 0.0590,
-        stemConst = 0.0771,
-        stemPower = 2.2704,
-        # pRx = 0.34,
-        # pRn = 0.13,
-        gammaF0 = 0,
-        gammaF1 = 0,
-        tgammaF = 0,
-        Rttover = 0.005,
-        m0 = 0,
-        fNo = 1,
-        fNn = 0,
-        coeffCond = 0.05,
-        maxAge = 50,
-        nAge = 4,
-        rAge = 0.95,
-        gammaN0 = 0,
-        gammaN1 = 0,
-        tgammaN = 0,
-        ngammaN = 1,
-        wSx1000 = 200,
-        thinPower = 1.5,
-        mF = 0,
-        mR = 0.2,
-        mS = 0.2,
-        SLA0 = 10.8,
-        SLA1 = 10.8,
-        tSLA = 1,
-        maxInterception = 0.15,
-        LAImaxInterception = 0,
-        fullCanAge = 0,
-        fracBB0 = 0,
-        fracBB1 = 0,
-        tBB = 0,
-        rhoMin = 0.358,
-        rhoMax = 0.358,
-        tRho = 4,
-        aH = 0.9740,
-        nHB = 0.6816,
-        nHN = 0.1064,
-        aV = 0.0001,
-        nVB = 2.3270,
-        nVN = 1.0915,
-        Qa = -90,
-        Qb = 0.8,
     )
 )
 
@@ -285,11 +146,149 @@ config_Model_Amichev = @config(
 #     )
 # )
 
-config_2 = @config(
-    config_Clock,
-    config_Calendar,
-    config_Atmosphere,
-    config_GasExchange_NI,
-    config_Model_Eucalyptus_globulus,
-    config_Model_Amichev
-)
+# config_2 = @config(
+#     config_Clock,
+#     config_Calendar,
+#     config_Atmosphere,
+#     config_GasExchange_NI,
+#     config_Model_Eucalyptus_globulus,
+#     config_Model_Amichev
+# )
+
+# Default configuration for canopy portion of the model.
+# config_Model_Amichev = @config(
+#     :Model => (
+#         # BiomassPartition
+#         # pFS2 = 0.8567,
+#         # pFS20 = 0.0590,
+#         stemConst = 0.0771,
+#         stemPower = 2.2704,
+#         # pRx = 0.34,
+#         # pRn = 0.13,
+#         gammaF0 = 0,
+#         gammaF1 = 0,
+#         tgammaF = 0,
+#         Rttover = 0.005,
+#         m0 = 0,
+#         fNo = 1,
+#         fNn = 0,
+#         coeffCond = 0.05,
+#         maxAge = 50,
+#         nAge = 4,
+#         rAge = 0.95,
+#         gammaN0 = 0,
+#         gammaN1 = 0,
+#         tgammaN = 0,
+#         ngammaN = 1,
+#         wSx1000 = 200,
+#         thinPower = 1.5,
+#         mF = 0,
+#         mR = 0.2,
+#         mS = 0.2,
+#         SLA0 = 10.8,
+#         SLA1 = 10.8,
+#         tSLA = 1,
+#         maxInterception = 0.15,
+#         LAImaxInterception = 0,
+#         fullCanAge = 0,
+#         fracBB0 = 0,
+#         fracBB1 = 0,
+#         tBB = 0,
+#         rhoMin = 0.358,
+#         rhoMax = 0.358,
+#         tRho = 4,
+#         aH = 0.9740,
+#         nHB = 0.6816,
+#         nHN = 0.1064,
+#         aV = 0.0001,
+#         nVB = 2.3270,
+#         nVN = 1.0915,
+#         Qa = -90,
+#         Qb = 0.8,
+#     )
+# )
+
+# 
+# config_Model_Eucalyptus_globulus = @config(
+#     :Model => ( 
+#         # BiomassPartition
+#         # FR = 1,
+#         # pFS2 = 1,
+#         # pFS20 = 0.15,
+#         aWs = 0.095,
+#         nWs = 2.4,
+#         # pRx = 0.8,
+#         # pRn = 0.25,
+#         # m0 = 0,
+#         coeffCond = 0.05,
+
+#         # Mortality
+#         gammaN1 = 3,
+#         gammaN0 = 0,
+#         tgammaN = 3,
+#         ngammaN = 1,
+#         wSx1000 = 400,
+#         thinPower = 1.5,
+#         mF = 0,
+#         mR = 0.2,
+#         mS = 0.2,
+#          
+#         # Tree
+#         iStemNo= 1111,
+#         fracBB0 = 0.75,
+#         fracBB1 = 0.15,
+#         tBB = 2,
+#         rho0 = 0.450,
+#         rho1 = 0.450,
+#         tRho = 4,
+#         aH = 0,
+#         nHB = 0,
+#         nHN = 0,
+#         aV = 0,
+#         nVB = 0,
+#         nVN = 0,
+#          
+#         # Foliage
+#         iWF = 2000,
+#         SLA0= 11,
+#         SLA1= 4,
+#         tSLA= 2.5,
+#         gammaF1= 0.027,
+#         gammaF0= 0.001,
+#         tgammaF= 12,
+#         leaf_width= 3,
+#          
+#         # Stem
+#         iWS = 1000,
+#          
+#         # Root
+#         iWR = 2000,
+#         gammaR= 0.015,
+
+#         # Radiation
+#         leaf_angle = ellipsoidal,
+#         LAF = 3,
+#         scattering = 0.15,
+#         clumping = 1.0,
+
+#         # Age
+#         iAge = 1,
+#         maxAge = 50,
+#         nAge = 4,
+#         rAge = 0.95,
+
+#         # WaterBalance
+#         iASW = 999,
+#         maxASW = 300,
+#         minASW = 0,
+#         irrigation = 0,
+#         pool_fraction = 0,
+#         maxInterception = 0.15,
+#         LAImaxInterception = 0,
+#         SWconst0 = 0.7,
+#         SWpower0 = 9,
+
+#         # Soil
+#         soil_class = CL,
+#     )
+# )
