f4b1fe26ae7f19d4780fb2def009b1ca98e74772
Junhyuk Jeon <jnhykjeon@gmail.com>
2024-11-14 08:46:46 +0900

    Change field_capacity function

--- src/rhizosphere/waterbalance.jl
+++ src/rhizosphere/waterbalance.jl
@@ -43,7 +43,8 @@ Transpiration
     #     ((Int(soil_class) > 0) ? (11 - 2 * Int(soil_class)) : (SWpower0))
     # end ~ preserve
 
-    field_capacity(maxASW) => 0.5 * maxASW ~ preserve(u"mm")
+    fc => 0.5 ~ preserve(parameter)
+    field_capacity(fc, maxASW) => fc * (maxASW + minASW) ~ preserve(u"mm")
 
     "Proportion of rain intercepted"
     interception(LAI, maxInterception, LAImaxInterception) => begin
