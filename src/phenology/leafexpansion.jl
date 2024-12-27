7f121769cc9b77c8553e086a7d6be025a30abcf5
vcorcoro <137559813+vcorcoro@users.noreply.github.com>
2024-12-27 11:00:59 -0800

    no leaf expansion during senescence

--- src/phenology/leafexpansion.jl
+++ src/phenology/leafexpansion.jl
@@ -3,7 +3,7 @@
     leaf_max => 2e3 ~ preserve(parameter, u"kg/ha")
 
     # Budburst only when forcing requirement met. No budburst when coppiced i.e. WS == 0.
-    leafexpansion(bud_max, leaf_max, WF) => begin
-        (WF > bud_max) && (WF <= leaf_max)
+    leafexpansion(bud_max, leaf_max, WF, senescent) => begin
+        (WF > bud_max) && (WF <= leaf_max) && !senescent
     end ~ flag
 end
