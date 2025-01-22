8715117b32b8683ea023a0583eb74eb16930da9f
szerrade-uw <69888530+szerrade-uw@users.noreply.github.com>
2025-01-22 10:44:57 -0800

    Add files via upload

--- src/physiology/photosynthesis.jl
+++ src/physiology/photosynthesis.jl
@@ -10,10 +10,10 @@ Photosynthesis
     =================#
 
     "Gas exchange model for sunlit leaves"
-    sunlit_gasexchange(context, PPFD=Q_sun, LAI=LAI_sunlit, w=leaf_width) ~ ::GasExchange
+    sunlit_gasexchange(context, PPFD=Q_sun, LAI=LAI_sunlit, w=leaf_width,drought_factor =drought_factor) ~ ::GasExchange
 
     "Gas exchange model for shaded leaves"
-    shaded_gasexchange(context, PPFD=Q_sh, LAI=LAI_shaded, w=leaf_width) ~ ::GasExchange
+    shaded_gasexchange(context, PPFD=Q_sh, LAI=LAI_shaded, w=leaf_width, drought_factor = drought_factor) ~ ::GasExchange
 
     #=================
     =================#
