4d7b8fc9c14451bf9b3c431912ab71cae5af80e1
szerrade-uw <69888530+szerrade-uw@users.noreply.github.com>
2025-01-28 12:45:03 -0800

    Add files via upload

--- src/phenology/shooting.jl
+++ src/phenology/shooting.jl
@@ -17,6 +17,22 @@
 	365
     end ~ preserve(parameter,u"d")
 
+    root_shoot_ratio(WR,WS,WF) => begin
+        (WR)/(WS+WF)
+    end ~ track()
+
+    min_RSR:min_root_shoot_ratio => begin
+         .1
+    end ~ preserve(parameter)
+
+    percent_NSC => begin
+	.3
+    end ~ preserve(parameter)
+
+    non_structural_carbon(WR,percent_NSC) => begin
+        percent_NSC*WR
+    end ~ track(u"kg/ha")
+
     ShD(T_air, T_shoot, T_shoot_opt): shooting_degrees => begin
         min(T_air, T_shoot_opt) - T_shoot 
     end ~ track(when=shooting, min=0, u"K")
@@ -24,10 +40,16 @@
     shoot_rate => 2 ~ preserve(parameter, u"kg/ha/hr/K")
 
     # Maximum shoot growth available for coppicing based on available root drymass.
-    dShoot_max(WR, step) => WR / step ~ track(u"kg/ha/hr")
+    dShoot_max(WR, step,non_structural_carbon) => non_structural_carbon / step ~ track(u"kg/ha/hr")
 
     # Hourly shoot growth rate.
-    dShoot(shoot_rate, ShD) => shoot_rate * ShD ~ track(max=dShoot_max, u"kg/ha/hr")
+    dShoot(shoot_rate, ShD, root_shoot_ratio,non_structural_carbon,shoot,min_RSR) => begin 
+        if(root_shoot_ratio<min_RSR || shoot>=non_structural_carbon) 
+	   0
+        else
+           shoot_rate * ShD 
+        end
+    end ~ track(max=dShoot_max, u"kg/ha/hr")
 
     # Accumulated shoot growth for the season, resets every year.
     shoot(dShoot) ~ accumulate(reset=senescent, u"kg/ha")
