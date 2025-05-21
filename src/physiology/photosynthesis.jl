7ff594e71064c9d38c22f6948ca88546cf21fa32
vcorcoro <137559813+vcorcoro@users.noreply.github.com>
2025-05-21 10:45:36 -0700

    Update npp_annual units

--- src/physiology/photosynthesis.jl
+++ src/physiology/photosynthesis.jl
@@ -88,7 +88,7 @@ Photosynthesis
     #    γ*GPP
     #end ~ track(u"kg/ha/hr")
 
-    NPP_annual(NPP) ~ accumulate(reset=dormant)
+    NPP_annual(NPP) ~ accumulate(reset=dormant, u"g/ha")
 
     "Water-use efficiency"
     WUE(NPP, transpiration) => begin
