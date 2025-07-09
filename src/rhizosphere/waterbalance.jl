e097c1291ba90e6845e5e3ab48530bbb8528f35e
vcorcoro <137559813+vcorcoro@users.noreply.github.com>
2025-07-09 12:35:21 -0700

    accumulate over years instead of reset

--- src/rhizosphere/waterbalance.jl
+++ src/rhizosphere/waterbalance.jl
@@ -214,11 +214,11 @@ Transpiration
     end ~ track(min=0.1, max=1) 
         
     # annual cumulative water usage for comparing WUE
-    IR_ac(irrigation) ~ accumulate(reset=budburst, u"L/ha")
+    IR_ac(irrigation) ~ accumulate(when=!dormant, u"L/ha")
     IR_annual(IR_ac) ~ remember(when=dormant, u"L/ha")
-    T_ac(transpiration) ~ accumulate(reset=budburst, u"L/ha")
+    T_ac(transpiration) ~ accumulate(when=!dormant, u"L/ha")
     T_annual(T_ac) ~ remember(when=dormant, u"L/ha")
-    ET_ac(evapotranspiration) ~ accumulate(reset=budburst, u"L/ha")
+    ET_ac(evapotranspiration) ~ accumulate(when=!dormant, u"L/ha")
     ET_annual(ET_ac) ~ remember(when=dormant, u"L/ha")
     
 end
