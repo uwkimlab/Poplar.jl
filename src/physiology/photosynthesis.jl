bfceb39d6f0f7638955bd0a8c65b69061a574723
vcorcoro <137559813+vcorcoro@users.noreply.github.com>
2026-01-29 16:15:13 -0800

    add calculated npp/gpp ratio for comparison

--- src/physiology/photosynthesis.jl
+++ src/physiology/photosynthesis.jl
@@ -96,6 +96,10 @@ Photosynthesis
 
     GPP_ac(GPP) ~ accumulate(u"kg/ha")
 
+    # output NPP/GPP ratio for comparison when NPP_type = 2
+    γ_out(NPP, GPP) => NPP / GPP ~ track
+    γ_out_ac(NPP_ac, GPP_ac) => NPP_ac / GPP_ac ~ track
+
     "Water-use efficiency"
     WUE(NPP, transpiration) => begin
         NPP / transpiration
