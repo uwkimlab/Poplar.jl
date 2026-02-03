f9c80e5ab1bb7d72351948388b6a4d91e64597ed
vcorcoro <137559813+vcorcoro@users.noreply.github.com>
2026-02-03 12:12:01 -0800

    leaf_max as function of bud_max

--- src/phenology/leafexpansion.jl
+++ src/phenology/leafexpansion.jl
@@ -1,7 +1,7 @@
 @system LeafExpansion begin
 
-    leaf_max_percent => 0.25 ~ preserve(parameter)
-    leaf_max(WD, leaf_max_percent) => leaf_max_percent * WD ~ track(u"kg/ha")
+    leaf_max_factor => 2 ~ preserve(parameter)
+    leaf_max(bud_max, leaf_max_factor) => leaf_max_factor * bud_max ~ track(u"kg/ha")
 
     # Budburst only when forcing requirement met. No budburst when coppiced i.e. WS == 0.
     leafexpansion(bud_max, leaf_max, WF, senescent) => begin
