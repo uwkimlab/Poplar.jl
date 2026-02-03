98952c02d8e1e05737bedb2812d43d1b5c65f53a
vcorcoro <137559813+vcorcoro@users.noreply.github.com>
2026-02-03 12:06:36 -0800

    Update leafexpansion.jl

--- src/phenology/leafexpansion.jl
+++ src/phenology/leafexpansion.jl
@@ -1,8 +1,5 @@
 @system LeafExpansion begin
 
-    dWD(W, x=context.clock.step) => W/x^2 ~ capture(u"kg/ha/hr", when=dormant)
-    WD(dWD): dormant_biomass ~ accumulate(u"kg/ha", max=W)
-
     leaf_max_percent => 0.25 ~ preserve(parameter)
     leaf_max(WD, leaf_max_percent) => leaf_max_percent * WD ~ track(u"kg/ha")
 
