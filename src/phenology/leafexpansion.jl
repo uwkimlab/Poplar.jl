535dcc267f2330128956ff67ac09ebcb680e440b
vcorcoro <137559813+vcorcoro@users.noreply.github.com>
2026-02-03 11:08:48 -0800

    leaf_max function of dormant biomass

--- src/phenology/leafexpansion.jl
+++ src/phenology/leafexpansion.jl
@@ -1,6 +1,10 @@
 @system LeafExpansion begin
 
-    leaf_max => 2e3 ~ preserve(parameter, u"kg/ha")
+    dWD(dW) ~ capture(u"kg/ha/hr", when=dormant)
+    WD(dWD): dormant_biomass ~ accumulate(u"kg/ha", max=W)
+
+    leaf_max_percent => 0.25
+    leaf_max(WD, leaf_max_percent) => leaf_max_percent * WD ~ preserve(parameter, u"kg/ha")
 
     # Budburst only when forcing requirement met. No budburst when coppiced i.e. WS == 0.
     leafexpansion(bud_max, leaf_max, WF, senescent) => begin
